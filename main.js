184f5e18b20788f503b519637342bb4ee8325a9e
tank <35127914+tank0001@users.noreply.github.com>
2020-10-28 17:56:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ client.on("message", async message => {
   // ATTENTION !!!!!!! THIS IS WHERE YOU CAN CHANGE THE STATUS LOOK RIGHT BELOW
   // you can change the online thingy to 'online', 'idle', or 'dnd'
   // you can also change 'WATCHING' to 'PLAYING' or 'LISTENING'
-  client.user.setPresence({ activity: { name: 'cuddles until you melt 😁', type: 'PLAYING'}, status: 'idle' })
+  client.user.setPresence({ activity: { name: 'kisses all over😁', type: 'PLAYING'}, status: 'idle' })
   
  if(message.author.bot) return;
 
