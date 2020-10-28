dec781b020791f6c750105f315a754c5a1338393
tank <35127914+tank0001@users.noreply.github.com>
2020-10-28 12:06:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 client.on("message", async message => {
   // ATTENTION !!!!!!! THIS IS WHERE YOU CAN CHANGE THE STATUS LOOK RIGHT BELOW
   // you can change the online thingy to 'online', 'idle', or 'dnd'
-  client.user.setPresence({ activity: { name: 'test', type: 'WATCHING'}, status: 'idle' })
+  client.user.setPresence({ activity: { name: 'your eyes 😁', type: 'WATCHING'}, status: 'idle' })
   
  if(message.author.bot) return;
 
