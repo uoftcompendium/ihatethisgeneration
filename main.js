394b2812ff6ee541dd9896dc33e0e0471578b6b6
kaischmoney <73150682+kaischmoney@users.noreply.github.com>
2020-11-17 11:50:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,8 @@ client.on("message", async message => {
    }
 
  if (message.content.toLowerCase().includes(`lolo`)) {
-     message.channel.send("simping grace :rolling_eyes:");
+     message.channel.send("Look. This is all I have to say.
+We should be creating a safe and welcoming community. How can we do that if when people are speaking up about theyre abuse (WHICH THEY SHOULDNT BE DEALING WITH HERE ANYWAYS)  they’re shot down and fully invalidated?");
    }
 
  if (message.content.toLowerCase().includes(`who wont me`)) {
