2f606f8685778145b95f335e0c6faba606b817bc
tank <35127914+tank0001@users.noreply.github.com>
2020-12-23 17:16:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ client.on("message", async message => {
   // you can also change 'WATCHING' to 'PLAYING' or 'LISTENING'
   client.user.setPresence({ activity: { name: 'thinking of you 💃', type: 'PLAYING'}, status: 'idle' })
   
-
+ if(message.author.bot) return;
   
   
 // this is where the fun begins
