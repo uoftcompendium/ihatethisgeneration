0bcf82c9ffc7ce8635c461cd2daa44eb555052a5
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2021-01-05 21:56:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ const { token, prefix } = require('./config.json');
 const client = new Discord.Client();
 //
 client.on("message", async message => {
-  // ATTENTION !!!!!!! THIS IS WHERE YOU CAN CHANGE THE STATUS LOOK RIGHT BELOW
-  // you can change the online thingy to 'online', 'idle', or 'dnd'
-  // you can also change 'WATCHING' to 'PLAYING' or 'LISTENING'
-  //client.user.setPresence({ activity: { name: 'happy thoughts😁', type: 'PLAYING'}, status: 'idle' })
   
  if(message.author.bot) return;
   
